460e607ef11c3b42d1692f041e08e547f8b230bb
artisyn <artioms1@gmail.com>
2022-03-20 19:37:05 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import './App.scss';
+import axios from 'axios';
 
 function App() {
+	const [title, setTitle] = useState('Hello React');
+	const [pokemon, setPokemon] = useState({});
+	const [inputValue, setInputValue] = useState('');
+
+	const showPokemon = async () => {
+		const request = await axios.get(
+			`https://pokeapi.co/api/v2/pokemon/${inputValue}`
+		);
+		console.log(request);
+	};
+
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<header>
+				<input
+					type="text"
+					onChange={(e) => {
+						setInputValue(e.target.value);
+						console.log(e.target.value);
+					}}
+				/>
+				<h1> {title}</h1>
+			</header>
+			<button onClick={showPokemon}>Show Pokemon</button>
+			<section></section>
 		</div>
 	);
 }
