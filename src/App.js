420873990456f2ca23cac28861b306ae83df010d
artisyn <artioms1@gmail.com>
2022-03-21 16:31:12 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import Axios from 'axios';
 import './App.scss';
 import axios from 'axios';
 
@@ -13,7 +12,15 @@ function App() {
 		const request = await axios.get(
 			`https://pokeapi.co/api/v2/pokemon/${inputValue}`
 		);
+		setTitle(request.data.name);
 		console.log(request);
+		setPokemon({
+			name: request.data.name,
+			picture: request.data.sprites.front_default,
+			species: request.data.species.name,
+		});
+
+		console.log(pokemon);
 	};
 
 	return (
@@ -23,13 +30,22 @@ function App() {
 					type="text"
 					onChange={(e) => {
 						setInputValue(e.target.value);
-						console.log(e.target.value);
 					}}
 				/>
 				<h1> {title}</h1>
 			</header>
 			<button onClick={showPokemon}>Show Pokemon</button>
-			<section></section>
+			<section>
+				{!pokemon.name ? (
+					'Please choose a pokemon'
+				) : (
+					<>
+						<h2>{'name ' + pokemon.name}</h2>
+						<h3>Species: {pokemon.species}</h3>
+						<img src={pokemon.picture} alt="pokemon" />
+					</>
+				)}
+			</section>
 		</div>
 	);
 }
